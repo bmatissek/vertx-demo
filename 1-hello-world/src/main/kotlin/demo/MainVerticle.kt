117133bfb94d0db50e4dc9f7517d7d87f265036f
Arthur Fernandes Araujo <arthur@cds.eb.mil.br>
2018-05-21 11:14:59 -0300

    Hello world kotlin multiproject build

--- 1-hello-world/src/main/kotlin/demo/MainVerticle.kt
+++ 1-hello-world/src/main/kotlin/demo/MainVerticle.kt
@@ -1,11 +1,19 @@
 package demo
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.Vertx
 
 fun main(args : Array<String>) {
+    val vertx = Vertx.vertx()
+    vertx.deployVerticle(MainVerticle())
 }
 
 class MainVerticle : AbstractVerticle() {
     override fun start() {
+        vertx.createHttpServer()
+            .requestHandler({request ->
+                request.response().end("Hello World")
+            })
+            .listen(8080)
     }
 }
